57aecd4f9678c3fa641978fc99367a13c90a2a00
GilbertXiao <gilxyj@163.com>
2020-01-05 22:40:49 +0800

    change to multiModule

---
+++ gytvhr/gytmailserver/src/main/java/com/gilxyj/gytmailserver/GytmailserverApplication.java
@@ -0,0 +1,13 @@
+package com.gilxyj.gytmailserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GytmailserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GytmailserverApplication.class, args);
+    }
+
+}
---
+++ gytvhr/gytmailserver/src/test/java/com/gilxyj/gytmailserver/GytmailserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilxyj.gytmailserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GytmailserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
